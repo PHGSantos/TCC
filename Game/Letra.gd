c1bdfc384c059e270d31962ccc4e079db537fa87
Pedro Henrique Gomes dos Santos <peuh1997@gmail.com>
2021-04-09 01:57:39 -0300

    começando o jogo da memória

---
+++ Game/Letra.gd
@@ -0,0 +1,32 @@
+extends TextureRect
+#extends TextureButton
+class_name Letra
+
+var color
+var value
+var face
+var back
+
+func _ready():
+	pass
+	
+func get_resized_texture(t: Texture, width: int = 0, height: int = 0):
+	var image = t.get_data()
+	if width > 0 && height > 0:
+		image.resize(width, height)
+	var itex = ImageTexture.new()
+	itex.create_from_image(image)
+	return itex	
+	
+func _init(var c, var v):
+	color = c
+	value = v
+	#face = load('res://letter_tiles/PNG/'+color+'/'+value+'.png')
+	face = load('res://letter_tiles/PNG/Box/'+value+'.png')
+	back = MemoriaController.cardBack
+	set_size(Vector2(20,20))
+	face = get_resized_texture(face,128,128)
+	self.texture = face	
+	#set_normal_texture(face)
+	
+	
---
+++ Game/Memoria.gd
@@ -0,0 +1,7 @@
+extends Control
+
+
+func _ready():
+#	var l = Letra.new('Blue', 'letter_A')
+#	$grid.add_child(l)
+	pass
