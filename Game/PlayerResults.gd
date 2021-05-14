16175f8081e648a8de0395777e96838c164afe1d
Pedro Gomes <peuh1997@gmail.com>
2021-05-14 15:00:40 -0300

    att3

---
+++ Game/PlayerResults.gd
@@ -0,0 +1,19 @@
+extends Node
+
+var j1_omission_errors
+var j1_comission_errors
+var j1_hit_reaction_time
+var j1_detectability
+var j1_response_style
+var j1_persevaration
+var j1_block_reaction_time
+
+var j2_total_response_time
+var j2_correct_answers
+var j2_incorrect_answers
+var j2_average_response_time
+
+var j3_points_per_step
+
+func _ready():
+	pass
